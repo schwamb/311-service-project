a2f5dd6d86a88ccdf7bcea639bd999d6ec74ba19
Susan Chambers <chamberssu@gmail.com>
2021-05-24 19:37:19 -0500

    Added joblib

---
+++ app.py
@@ -0,0 +1,42 @@
+#import libraries
+import numpy as np
+from flask import Flask, render_template,request, jsonify
+from flask_cors import CORS
+from joblib import load
+app = Flask(__name__)
+CORS(app)
+
+reg = load("model")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = reg.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Turnaround time should be {} days'.format(output))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
